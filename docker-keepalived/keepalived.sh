69da3708b5e328824b61b6670419718a8a4d34c6
cavemandaveman <cavemandaveman@openmailbox.org>
2016-11-22 10:52:42 -0600

    Fix VIRTUAL_IP regex

--- docker-keepalived/keepalived.sh
+++ docker-keepalived/keepalived.sh
@@ -29,7 +29,7 @@ stop()
 
 # Make sure the variables we need to run are populated and (roughly) valid
 
-if ! [[ $VIRTUAL_IP =~ ^(([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-5][0-5])\.){3}([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-5][0-5])$ ]]; then
+if ! [[ $VIRTUAL_IP =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]; then
   echo "The VIRTUAL_IP environment variable is null or not a valid IP address, exiting..."
   exit 1
 fi
